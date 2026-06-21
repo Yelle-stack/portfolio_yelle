a04f4803e008e63f3f0136e9287afda0277f5b70
Jelena <jelena33.bliss@gmail.com>
2026-06-21 13:32:29 +0200

    navbarChangement

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,74 +1,108 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.PNG'
-import underline from '../../assets/nav_underline.svg'
-import menu_open from '../../assets/menu_open.svg'
-import menu_close from '../../assets/menu_close.svg'
+import React, { useState, useRef } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.PNG';
+import underline from '../../assets/nav_underline.svg';
+import menu_open from '../../assets/menu_open.svg';
+import menu_close from '../../assets/menu_close.svg';
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("home");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+    setIsMenuOpen(true);
+  };
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+    setIsMenuOpen(false);
+  };
+
+  const handleMenuClick = (section) => {
+    setMenu(section);
+    closeMenu();
+  };
 
   return (
-    <div className='navbar'>
-      <img src={logo} alt="logo_photo" className='logo'/>
-      <img src={menu_open} alt="open" className='nav-mob-open'/>
+    <div className="navbar">
 
-      <ul className="nav-menu">
-         <img src={menu_close} alt="close" className="nav-mob-close" />
+      <img src={logo} alt="logo" className="logo" />
+
+      {!isMenuOpen && (
+        <img
+          src={menu_open}
+          alt="Open Menu"
+          className="nav-mob-open"
+          onClick={openMenu}
+        />
+      )}
+
+      <ul ref={menuRef} className="nav-menu">
+
+        <img
+          src={menu_close}
+          alt="Close Menu"
+          className="nav-mob-close"
+          onClick={closeMenu}
+        />
+
         <li>
           <a
             href="#home"
             className="anchor-link"
-            onClick={() => setMenu("home")}
+            onClick={() => handleMenuClick("home")}
           >
             Home
           </a>
-          {menu === "home" ? <img src={underline} alt="" /> : null}
+          {menu === "home" && <img src={underline} alt="" />}
         </li>
 
         <li>
           <a
             href="#about"
             className="anchor-link"
-            onClick={() => setMenu("about")}
+            onClick={() => handleMenuClick("about")}
           >
             About Me
           </a>
-          {menu === "about" ? <img src={underline} alt="" /> : null}
+          {menu === "about" && <img src={underline} alt="" />}
         </li>
 
         <li>
           <a
             href="#services"
             className="anchor-link"
-            onClick={() => setMenu("services")}
+            onClick={() => handleMenuClick("services")}
           >
             Services
           </a>
-          {menu === "services" ? <img src={underline} alt="" /> : null}
+          {menu === "services" && <img src={underline} alt="" />}
         </li>
 
         <li>
           <a
             href="#work"
             className="anchor-link"
-            onClick={() => setMenu("work")}
+            onClick={() => handleMenuClick("work")}
           >
             Portfolio
           </a>
-          {menu === "work" ? <img src={underline} alt="" /> : null}
+          {menu === "work" && <img src={underline} alt="" />}
         </li>
 
         <li>
           <a
             href="#contact"
             className="anchor-link"
-            onClick={() => setMenu("contact")}
+            onClick={() => handleMenuClick("contact")}
           >
             Contact
           </a>
-          {menu === "contact" ? <img src={underline} alt="" /> : null}
+          {menu === "contact" && <img src={underline} alt="" />}
         </li>
 
       </ul>
@@ -76,8 +110,9 @@ const Navbar = () => {
       <a href="#contact" className="nav-connect">
         Connect With Me
       </a>
-    </div>
-  )
-}
 
-export default Navbar
+    </div>
+  );
+};
+
+export default Navbar;
