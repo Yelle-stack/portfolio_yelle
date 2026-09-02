2148eadff7a4a9d46e5b71003c95166e56e8ff54
Jelena <jelena33.bliss@gmail.com>
2026-09-02 19:33:04 +0200

    src

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/profilpicture.png'
+import profile_img from '../../assets/profilepicture.png'
 
 const About = () => {
   return (
